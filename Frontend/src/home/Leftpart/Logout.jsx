8f58a3fc09d8314bf6cc343d3f9a0c30cdf70b07
Siddhartha-306 <2021KUEC2034@iiitkota.ac.in>
2024-09-22 01:10:20 +0530

    added the logout button functionality

--- Frontend/src/home/Leftpart/Logout.jsx
+++ Frontend/src/home/Leftpart/Logout.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TbLogout } from "react-icons/tb";
+import axios from 'axios';
+import Cookies from "js-cookie";
 
 const Logout = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleLogout = async () => {
+    // setLoading(true);
+    try {
+      const response = await axios.post("/api/user/logout");
+      localStorage.removeItem("GupShup");
+      Cookies.remove("jwt");
+      // setLoading(false);
+      alert("Logged Out successfully");
+      window.location.reload();    //to reload the page so that after logout the login page will show
+    } catch (error) {
+      console.log("Error in logout", error);
+    }
+  }
+
   return (
     <div className='h-[6vh]'>
       <div className='flex text-white text-xl hover:bg-slate-700 duration-300 cursor-pointer p-1 h-[68px] justify-center items-center gap-4 mt-[29px]' onClick={handleLogout}>
