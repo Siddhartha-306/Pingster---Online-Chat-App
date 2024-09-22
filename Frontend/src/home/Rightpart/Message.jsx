eeb767b16ada1d833b7fc41f0204161d6ad59efd
Siddhartha-306 <2021KUEC2034@iiitkota.ac.in>
2024-09-23 04:59:15 +0530

    added the search functionality in the app

--- Frontend/src/home/Rightpart/Message.jsx
+++ Frontend/src/home/Rightpart/Message.jsx
@@ -8,11 +8,18 @@ const Message = ({message}) => {
   const chatName = itsMe ? "chat-end" : "chat-start";
   const chatColour = itsMe ? "bg-blue-500" : "";
   
+  const createdAt = new Date(message.createdAt);
+  const formattedTime = createdAt.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+
   return (
     <div>
       <div className='p-4'>
         <div className={`chat ${chatName}`}>
             <div className={`chat-bubble text-white ${chatColour}`}>{message.message}</div>
+            <div className='chat-footer'>{formattedTime}</div>
         </div>
       </div>
     </div>
